80d9ad781c64eca2816c526cc6483483ddbe8b44
phoene1990x@gmail.com <phoene1990x@gmail.com>
2021-12-17 04:51:45 +0100

    added @Query options to call added replacement {} options to call

--- app/src/main/java/app/web/markodunovic/retrofitexample/JsonPlaceHolderApi.java
+++ app/src/main/java/app/web/markodunovic/retrofitexample/JsonPlaceHolderApi.java
@@ -4,13 +4,32 @@ import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface JsonPlaceHolderApi {
 
     //base html url gets passed latter
     //this is just an endpoint
+    //if i use a querry then it must be the same as what is
+    //expected on the html sausage
     @GET("posts")
-    Call<List<Post>> getPosts();
+    Call<List<Post>> getPosts(
+            @Query("userId")Integer[] userId,
+            @Query("_sort")String sort,
+            @Query("_order")String order
+
+            // can Query like this or with the array as shown above
+            /*@Query("userId")int userId,
+            @Query("userId")int userId2,*/
+    );
     // call object contains a single requiest and a response
+
+    //{} is a replacement id
+    //@Path name and replacement id have to be the same
+    //what we call the variable does not mater
+
+    @GET("posts/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id")int postId);
 
 }
--- app/src/main/java/app/web/markodunovic/retrofitexample/MainActivity.java
+++ app/src/main/java/app/web/markodunovic/retrofitexample/MainActivity.java
@@ -2,10 +2,16 @@ package app.web.markodunovic.retrofitexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 
 import java.util.List;
@@ -19,6 +25,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+    private FloatingActionButton fab1,fab2;
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,38 +35,117 @@ public class MainActivity extends AppCompatActivity {
 
 
         textViewResult = findViewById(R.id.text_view_result);
+        fab1 = findViewById(R.id.fab_btn_1);
+        fab1.setImageBitmap(textAsBitmap("Post", 40, Color.WHITE));
+        fab1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewResult.setText("");
+                doBasicShit();
+            }
+        });
+        fab2 =findViewById(R.id.fab_btn_2);
+        fab2.setImageBitmap(textAsBitmap("COM",40,Color.WHITE));
+        fab2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textViewResult.setText("");
+                getKomentare();
+
+            }
+        });
+    }
+
+    private void getKomentare() {
+        // there was an error here dont forget to link retrofit and interface or it will be a null exception
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://jsonplaceholder.typicode.com/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        // its gonna bitch here because it now takes an argument for the {} and path
+        Call<List<Comment>> call = jsonPlaceHolderApi.getComments(3);
+        call.enqueue(new Callback<List<Comment>>() {
+            @Override
+            public void onResponse(Call<List<Comment>> call, Response<List<Comment>> response) {
+
+                if (!response.isSuccessful()){
+                    textViewResult.setText("Code: "+response.code());
+                    return;
+                }
+                List<Comment> comments = response.body();
+                for (Comment comment:comments){
+                    String content = "";
+                    content += "ID: "+ comment.getId()+"\n";
+                    content += "Post ID: "+ comment.getPostId()+"\n";
+                    content += "Name: "+ comment.getName()+"\n";
+                    content += "Email: "+ comment.getEmail()+"\n";
+                    content += "Text: "+ comment.getId()+"\n\n";
+                    textViewResult.append(content);
+
+                }
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Comment>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+
+    }
+
+    private void doBasicShit() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
-        Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        // can actually pass null for parameters they will just be ignored
+        // except for the integer since its not nullable
+        // Integer is nullable int is not nullable because Integer is a wrapper around int
+        Call<List<Post>> call = jsonPlaceHolderApi.getPosts(new Integer[]{1,3,5},"id","asc");
         call.enqueue(new Callback<List<Post>>() {
             @Override
             public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
-                    if (!response.isSuccessful()){
-                        textViewResult.setText("Code "+response.code());
-                        return;
-                    }
-                    List<Post> posts = response.body();
-                    for (Post post : posts){
-                        String content = "";
-                        content += "ID: "+post.getId() +"\n";
-                        content += "User ID: " +post.getUserId() +"\n";
-                        content += "Title: " + post.getTitle()+"\n";
-                        content += "Text: " +post.getText()+ "\n\n";
-                        Log.d("response", "onResponse: "+content);
-                        textViewResult.append(content);
+                if (!response.isSuccessful()){
+                    textViewResult.setText("Code "+response.code());
+                    return;
+                }
+                List<Post> posts = response.body();
+                for (Post post : posts){
+                    String content = "";
+                    content += "ID: "+post.getId() +"\n";
+                    content += "User ID: " +post.getUserId() +"\n";
+                    content += "Title: " + post.getTitle()+"\n";
+                    content += "Text: " +post.getText()+ "\n\n";
+                    Log.d("response", "onResponse: "+content);
+                    textViewResult.append(content);
 
-                    }
+                }
             }
 
             @Override
             public void onFailure(Call<List<Post>> call, Throwable t) {
-                    textViewResult.setText(t.getMessage());
+                textViewResult.setText(t.getMessage());
 
             }
         });
+    }
+
+    public static Bitmap textAsBitmap(String text, float textSize, int textColor) {
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setTextSize(textSize);
+        paint.setColor(textColor);
+        paint.setTextAlign(Paint.Align.LEFT);
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.0f); // round
+        int height = (int) (baseline + paint.descent() + 0.0f);
+        Bitmap image = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(image);
+        canvas.drawText(text, 0, baseline, paint);
+        return image;
     }
 }
